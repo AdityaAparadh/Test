f458d5e2ecbcfb1fac0895d51ee2d50510733d88
AdityaAparadh <aditya.aparadh.0@gmail.com>
2022-07-10 13:50:14 +0530

    - Options are clickable and functional - Added a Timer

--- assets/backend.js
+++ assets/backend.js
@@ -1,13 +1,19 @@
+window.onload = function () {
+    var time = 10800, // your time in seconds here
+        display = document.querySelector('#safeTimerDisplay');
+    startTimer(time, display);
+};
+
+
 var currentQuestion = 0;
 var currentSubject="Mathematics";
+var currentOption = "N";
 
 
-
-goToQuestion(2);
-
+nextQuestion();
 
 
-
+timer();
 
 
 
@@ -20,18 +26,23 @@ function displayQuestion(){
 }
 
 function nextQuestion(){
+    if(currentQuestion<30){
+    resetOption();
     currentQuestion++;
     displayQuestion();
-
+    }
 }
 
 function previousQuestion(){
-    currentQuestion--;
-    displayQuestion();
+    if(currentQuestion>1){
+        resetOption();
+        currentQuestion--;
+        displayQuestion();
+        }
 }
 
 function goToQuestion(qno){
-
+    resetOption();
     setCurrentQuestion(qno);
     displayQuestion();
 
@@ -48,3 +59,41 @@ function currentQuestionDisplay(){
 function currentQuestionDirectory(){
     return"assets/questions/" + currentSubject + "/" + currentQuestion +".png";
 }
+
+function setCurrentOption(op){
+    resetOption();
+
+    currentOption = op;
+    console.log(currentOption);
+    document.getElementById("option"+op).style.background= "#5dfc8d";
+}
+
+function resetOption(){
+    document.getElementById("optionA").style.background= "#e6e2c3";
+    document.getElementById("optionB").style.background= "#e6e2c3";
+    document.getElementById("optionC").style.background= "#e6e2c3";
+    document.getElementById("optionD").style.background= "#e6e2c3";
+    
+    currentOption = "N";
+}
+
+
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = "Time Remaining- " + minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = 0;
+            // timer = duration; // uncomment this line to reset timer automatically after reaching 0
+        }
+    }, 1000);
+}
+
