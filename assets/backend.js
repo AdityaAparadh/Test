74d1bc1d739cbc77851a91b0cc42c35618d8f457
AdityaAparadh <aditya.aparadh.0@gmail.com>
2022-07-10 16:36:17 +0530

    temp commit

--- assets/backend.js
+++ assets/backend.js
@@ -1,14 +1,18 @@
+
 window.onload = function () {
-    var time = 10800, // your time in seconds here
+        initializer();
+    var time = localStorage.getItem('timer'), // your time in seconds here
         display = document.querySelector('#safeTimerDisplay');
     startTimer(time, display);
+
 };
+
 
 
 var currentQuestion = 0;
 var currentSubject="Mathematics";
 var currentOption = "N";
-
+var currentSubjectCode = "m"
 
 nextQuestion();
 
@@ -21,14 +25,16 @@ timer();
 function displayQuestion(){
     document.getElementById('subject').innerHTML= currentSubject;
     document.getElementById('questionNumber').innerHTML= currentQuestionDisplay();    
-
     document.getElementById('mainQuestionDisplay').src = currentQuestionDirectory(); 
+
+    var ans = localStorage.getItem(currentSubject+currentQuestion);
+    document.getElementById("option"+ans).style.background= "#5dfc8d";
 }
 
 function nextQuestion(){
     if(currentQuestion<30){
-    resetOption();
     currentQuestion++;
+    resetOption();
     displayQuestion();
     }
 }
@@ -42,8 +48,9 @@ function previousQuestion(){
 }
 
 function goToQuestion(qno){
-    resetOption();
+    
     setCurrentQuestion(qno);
+    conditionalResetOption();
     displayQuestion();
 
 }
@@ -62,7 +69,7 @@ function currentQuestionDirectory(){
 
 function setCurrentOption(op){
     resetOption();
-
+    document.getElementById("savebutton").disabled = false;
     currentOption = op;
     console.log(currentOption);
     document.getElementById("option"+op).style.background= "#5dfc8d";
@@ -73,9 +80,36 @@ function resetOption(){
     document.getElementById("optionB").style.background= "#e6e2c3";
     document.getElementById("optionC").style.background= "#e6e2c3";
     document.getElementById("optionD").style.background= "#e6e2c3";
-    
+    document.getElementById("savebutton").disabled = true;
     currentOption = "N";
+    localStorage.setItem(currentSubject+currentQuestion, currentOption);
+    document.getElementById("m"+currentQuestion).style.background= "#00ffff" ;
 }
+
+function conditionalResetOption(){
+
+    if(localStorage.getItem(currentSubject+currentQuestion) = "N"){
+        resetOption();
+    } else{
+        document.getElementById("optionA").style.background= "#e6e2c3";
+        document.getElementById("optionB").style.background= "#e6e2c3";
+        document.getElementById("optionC").style.background= "#e6e2c3";
+        document.getElementById("optionD").style.background= "#e6e2c3";
+        document.getElementById("savebutton").disabled = true;
+        localStorage.setItem(currentSubject+currentQuestion, currentOption);
+        document.getElementById("m"+currentQuestion).style.background= "#00ffff" ;
+    }
+}
+
+function saveOption(){
+    localStorage.setItem(currentSubject+currentQuestion, currentOption);
+    document.getElementById("m"+currentQuestion).style.background= "#8bfa50";
+    nextQuestion();
+}
+
+
+
+
 
 
 
@@ -94,6 +128,28 @@ function startTimer(duration, display) {
             timer = 0;
             // timer = duration; // uncomment this line to reset timer automatically after reaching 0
         }
+        localStorage.setItem('timer', timer);
     }, 1000);
 }
 
+function initializer(){
+    var a = 1;
+    var b = 1;
+    var c= 1;
+
+    
+
+    while(a<=30){
+        localStorage.setItem("Mathematics"+a, 'N'); 
+        a++;
+    }
+
+    while(b<=30){
+        localStorage.setItem("Physics"+b, 'N');
+        b++;
+    }
+    while(c<=30){
+        localStorage.setItem("Chemistry"+c, 'N');
+        c++;
+    }
+}
