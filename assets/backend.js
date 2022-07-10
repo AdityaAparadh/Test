802d3311cb3dce32dfe44c963b69a7ab97201a6f
AdityaAparadh <aditya.aparadh.0@gmail.com>
2022-07-10 09:24:52 +0530

    - Added Grid - Basic CSS and JS

---
+++ assets/backend.js
@@ -0,0 +1,10 @@
+var currentQuestion = 1;
+var currentQuestionDisplay = "Question Number: " + currentQuestion;
+var currentSubject="Mathematics";
+
+document.getElementById('subject').innerHTML= currentSubject;
+
+document.getElementById('questionNumber').innerHTML= currentQuestionDisplay;
+
+
+console.log(currentQuestionDisplay);
