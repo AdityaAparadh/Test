303b4cf6932eb0861188e8ad1b58ada8b857a08e
AdityaAparadh <aditya.aparadh.0@gmail.com>
2022-07-10 11:44:24 +0530

    - Buttons Work - Lot of functions added - Radio Buttons barely added

--- assets/backend.js
+++ assets/backend.js
@@ -1,10 +1,50 @@
-var currentQuestion = 1;
-var currentQuestionDisplay = "Question Number: " + currentQuestion;
+var currentQuestion = 0;
 var currentSubject="Mathematics";
 
-document.getElementById('subject').innerHTML= currentSubject;
-
-document.getElementById('questionNumber').innerHTML= currentQuestionDisplay;
 
 
-console.log(currentQuestionDisplay);
+goToQuestion(2);
+
+
+
+
+
+
+
+
+function displayQuestion(){
+    document.getElementById('subject').innerHTML= currentSubject;
+    document.getElementById('questionNumber').innerHTML= currentQuestionDisplay();    
+
+    document.getElementById('mainQuestionDisplay').src = currentQuestionDirectory(); 
+}
+
+function nextQuestion(){
+    currentQuestion++;
+    displayQuestion();
+
+}
+
+function previousQuestion(){
+    currentQuestion--;
+    displayQuestion();
+}
+
+function goToQuestion(qno){
+
+    setCurrentQuestion(qno);
+    displayQuestion();
+
+}
+
+function setCurrentQuestion(no){
+currentQuestion = no;
+}
+
+
+function currentQuestionDisplay(){
+    return "Question Number: " + currentQuestion;
+}
+function currentQuestionDirectory(){
+    return"assets/questions/" + currentSubject + "/" + currentQuestion +".png";
+}
